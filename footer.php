bb0a576dce399b5aba037fa1b9d577d65893ce72
Jay Vardhan <vardhan.jay@gmail.com>
2024-12-03 12:27:41 +0530

    fixed search icon bugs

--- footer.php
+++ footer.php
@@ -6,54 +6,70 @@
 	</div>
 </div-->
 <footer class="top-buffer">
-<div class="container">
-	<div class="row">
-		<div class="col-md-12 text-center">
-			<ul class="list-inline share-icons">
-				<li>
-					<a href="https://www.facebook.com/Navayana/" target="_blank">
-					<i class="fa fa-facebook-official fa-2x"></i>
-					</a>
-				</li>
-				<!--li>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<ul class="list-inline share-icons">
+					<li>
+						<a href="https://www.facebook.com/Navayana/" target="_blank">
+							<i class="fa fa-facebook-official fa-2x"></i>
+						</a>
+					</li>
+					<!--li>
 					<a href="https://twitter.com/buffaloeskissin" target="_blank">
 					<i class="fa fa-twitter fa-2x"></i>
 					</a>
 				</li-->
-				<li>
-					<a href="https://www.google.co.in/maps/place/Navayana/@28.5475585,77.2152373,15z/data=!4m5!3m4!1s0x0:0x2adcc820693604c2!8m2!3d28.5475585!4d77.2152373" target="_blank"><i class="fa fa-map-marker fa-2x" aria-hidden="true"></i></a>
-				</li>
-			</ul>
+					<li>
+						<a href="https://www.google.co.in/maps/place/Navayana/@28.5475585,77.2152373,15z/data=!4m5!3m4!1s0x0:0x2adcc820693604c2!8m2!3d28.5475585!4d77.2152373" target="_blank"><i class="fa fa-map-marker fa-2x" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
-	<div class="row"> <!-- FOOTER MENU -->
-		<div class="col-sm-12 col-md-offset-1 col-md-10">
-		<hr>
-			<div class="footer-nav text-center">
-				<?php
+		<div class="row"> <!-- FOOTER MENU -->
+			<div class="col-sm-12 col-md-offset-1 col-md-10">
+				<hr>
+				<div class="footer-nav text-center">
+					<?php
 
-					wp_nav_menu( array(
-						'menu' 				=> 'footer',
-						'theme_location' 	=> 'footer',
-						'depth' 			=> 1,
-						'container' 		=> false,
-						'menu_class' 		=> 'nav navbar-nav',
-						'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-						'walker'            => new wp_bootstrap_navwalker())
+					wp_nav_menu(
+						array(
+							'menu' 				=> 'footer',
+							'theme_location' 	=> 'footer',
+							'depth' 			=> 1,
+							'container' 		=> false,
+							'menu_class' 		=> 'nav navbar-nav',
+							'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+							'walker'            => new wp_bootstrap_navwalker()
+						)
 
 					);
 
-				?>
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p class="copyright text-center top-buffer">Copyright Navayana Publishing Pvt. Ltd. 2021 | Developed on <a href="http://wordpress.org" target="_blank">Wordpress</a> by <a href="https://sputznik.com" target="_blank">Sputznik</a></p>
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<p class="copyright text-center top-buffer">Copyright Navayana Publishing Pvt. Ltd. 2021 | Developed on <a href="http://wordpress.org" target="_blank">Wordpress</a> by <a href="https://sputznik.com" target="_blank">Sputznik</a></p>
-		</div>
-	</div>
-</div>
 </footer>
 <?php wp_footer(); ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZBR667ZE92"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date());
+
+	gtag('config', 'G-ZBR667ZE92');
+</script>
+
 </body>
+
 </html>
--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-$(document).ready(function(){
-	$('.fa-search').click(function( event ){
-		event.preventDefault();
-		$(".searchform").slideToggle('slow');
-	});
+$(document).ready(function () {
+  $(".fa-search").click(function (event) {
+    event.preventDefault();
+    $(".searchform").slideToggle("slow");
+  });
 });
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZBR667ZE92"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZBR667ZE92');
-</script>
