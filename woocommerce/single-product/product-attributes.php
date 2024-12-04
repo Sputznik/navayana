9224222c9d76b6a81dfbcd1f76998a9bdfe76d71
Jay Vardhan <vardhan.jay@gmail.com>
2024-12-04 17:17:08 +0530

    updated product=attributes and product-image templates

--- woocommerce/single-product/product-attributes.php
+++ woocommerce/single-product/product-attributes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Product attributes
  *
@@ -14,61 +15,60 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.6.0
+ * @version 9.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 ?>
 
 <ul class="product-attributes list-inline">
-	<?php if ( $display_dimensions && $product->has_weight() ) : ?>
-	<li class="list-inline-item">
-		<?php echo '<span class="attribute-key">'.wc_attribute_label( 'Weight' ). ':</span>'; ?>
-		<span class="product_weight attribute-value"><?php echo esc_html( wc_format_weight( $product->get_weight() ) ); ?></span>
-	</li>
+	<?php if ($display_dimensions && $product->has_weight()) : ?>
+		<li class="list-inline-item">
+			<?php echo '<span class="attribute-key">' . wc_attribute_label('Weight') . ':</span>'; ?>
+			<span class="product_weight attribute-value"><?php echo esc_html(wc_format_weight($product->get_weight())); ?></span>
+		</li>
 	<?php endif; ?>
 
-	<?php if ( $display_dimensions && $product->has_dimensions() ) : ?>
+	<?php if ($display_dimensions && $product->has_dimensions()) : ?>
 		<li class="list-inline-item">
-			<?php echo '<span class="attribute-key">'. wc_attribute_label( 'Dimensions' ) .':</span>'; ?>
-			<span class="product_dimensions attribute-value"><?php echo esc_html( wc_format_dimensions( $product->get_dimensions( false ) ) ); ?></span>
+			<?php echo '<span class="attribute-key">' . wc_attribute_label('Dimensions') . ':</span>'; ?>
+			<span class="product_dimensions attribute-value"><?php echo esc_html(wc_format_dimensions($product->get_dimensions(false))); ?></span>
 		</li>
-	<?php endif; ?>	
+	<?php endif; ?>
 
-	<?php foreach ( $attributes as $attribute ) : ?>
-		
-			<li class="list-inline-item"><?php echo '<span class="attribute-key">'. wc_attribute_label( $attribute->get_name() ). ':</span>'; ?>
-				<?php
-				$values = array();
+	<?php foreach ($attributes as $attribute) : ?>
 
-				if ( $attribute->is_taxonomy() ) {
-					$attribute_taxonomy = $attribute->get_taxonomy_object();
-					$attribute_values = wc_get_product_terms( $product->get_id(), $attribute->get_name(), array( 'fields' => 'all' ) );
+		<li class="list-inline-item"><?php echo '<span class="attribute-key">' . wc_attribute_label($attribute->get_name()) . ':</span>'; ?>
+			<?php
+			$values = array();
 
-					foreach ( $attribute_values as $attribute_value ) {
-						$value_name = esc_html( $attribute_value->name );
+			if ($attribute->is_taxonomy()) {
+				$attribute_taxonomy = $attribute->get_taxonomy_object();
+				$attribute_values = wc_get_product_terms($product->get_id(), $attribute->get_name(), array('fields' => 'all'));
 
-						if ( $attribute_taxonomy->attribute_public ) {
-							$values[] = '<a href="' . esc_url( get_term_link( $attribute_value->term_id, $attribute->get_name() ) ) . '" rel="tag">' . $value_name . '</a>';
-						} else {
-							$values[] = $value_name;
-						}
-					}
-				} else {
-					$values = $attribute->get_options();
+				foreach ($attribute_values as $attribute_value) {
+					$value_name = esc_html($attribute_value->name);
 
-					foreach ( $values as &$value ) {
-						$value = make_clickable( $value );
+					if ($attribute_taxonomy->attribute_public) {
+						$values[] = '<a href="' . esc_url(get_term_link($attribute_value->term_id, $attribute->get_name())) . '" rel="tag">' . $value_name . '</a>';
+					} else {
+						$values[] = $value_name;
 					}
 				}
+			} else {
+				$values = $attribute->get_options();
 
-				echo '<span class="attribute-value">'.apply_filters( 'woocommerce_attribute',  wptexturize( implode( ', ', $values ) ), $attribute, $values ).'</span>';
+				foreach ($values as &$value) {
+					$value = make_clickable($value);
+				}
+			}
+
+			echo '<span class="attribute-value">' . apply_filters('woocommerce_attribute',  wptexturize(implode(', ', $values)), $attribute, $values) . '</span>';
 			?>
 		</li>
-		
+
 	<?php endforeach; ?>
 
-</ul>	
-
+</ul>
